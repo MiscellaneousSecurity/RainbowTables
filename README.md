a35c91962429001ffa5e7a68299f13b1d1eb2dac
Adam Kudelas <111202841+LittleBread69@users.noreply.github.com>
2025-12-01 09:19:38 +0100

    trying to fix issues with lfs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# RainbowTables
+version https://git-lfs.github.com/spec/v1
+oid sha256:d2dbc84e714e85ffab5408bd969a970b495553e26ae3ed055ee0d3f7bafe4e9d
+size 16
--- passwordfetcher.py
+++ passwordfetcher.py
@@ -1,15 +1,3 @@
-'''This is the password fetcher.'''
-import lzma
-
-
-def fetch_passwords(password_xz_file_name:str = "password-list.txt.xz") -> set[bytes]: 
-    ''' '''
-    with lzma.open(f"passwords/{password_xz_file_name}", 'rb') as pswf:
-        pass_set = set(())
-        for line in pswf.readlines():
-            line = line.strip(b"\n")
-            pass_set.add(line)
-        return pass_set
-
-if __name__ == '__main__':
-    print(fetch_passwords())
+version https://git-lfs.github.com/spec/v1
+oid sha256:b6a12029ac3ef507441ce8bc684b1266b6f4beb4234304453ff1e9965ab611b1
+size 741
